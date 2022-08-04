51c5bc4b9099e25ab578fdc58bfb55464645589f
ViniciusGP <dota2chanel666@gmail.com>
2022-08-03 22:07:07 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {TodoList} from './components/TodoList'
+import {NewTodo} from './components/NewTodo'
+import { useState } from 'react';
 
 function App() {
+
+  const [todos, setTodos]= useState([])
+  
+  const checkTodo =(id) => {
+    setTodos(prev => {
+
+      const newArray = [...prev].map(todo =>{
+        if(todo.id !== id) return todo;
+        return{
+          ...todo,
+          isChecked: !todo.isChecked,
+        };
+      });
+      
+      return newArray;
+    }
+
+      
+      )}
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Lista de tarefas</h1>
+     <NewTodo setTodos={setTodos}/>
+      <TodoList todos={todos} checkTodo={checkTodo}
+      />
     </div>
   );
 }
